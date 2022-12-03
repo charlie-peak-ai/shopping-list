1819ade97552f57ed9de777fddf7a22aa06df715
Charlie <charlie.entwistle@peak.ai>
2022-12-03 08:44:10 +0000

    Added entries Updated entries

--- Shopping/recipes/ingredients/dry_goods.py
+++ Shopping/recipes/ingredients/dry_goods.py
@@ -1,4 +1,6 @@
-from Shopping.recipes.core import Ingredient, Categories as c, Quantities as q
+from Shopping.recipes.core import Categories as c
+from Shopping.recipes.core import Ingredient
+from Shopping.recipes.core import Quantities as q
 
 TinLentils = Ingredient(name="Tin Lentils", category=c.Tinned, est_price=0.5, quantity=q.Tin)
 TinTomatoes = Ingredient(name="Tin Tomatoes", category=c.Tinned, est_price=0.5, quantity=q.Tin)
@@ -6,8 +8,9 @@ TinBlackBean = Ingredient(name="Tin Black Beans", category=c.Tinned, est_price=0
 CoconutMilk = Ingredient(name="Tin Coconut Milk", category=c.Tinned, est_price=0.5, quantity=q.Tin)
 KidneyBeans = Ingredient(name="Kidney Beans", category=c.Tinned, est_price=1, quantity=q.Tin)
 
-Rice = Ingredient(name="Rice", category=c.DryGood, est_price=1, quantity=500)
-Pasta = Ingredient(name="Pasta", category=c.DryGood, est_price=1, quantity=500)
+Rice = Ingredient(name="Rice", category=c.DryGood, est_price=1, quantity=q.Pack)
+Pasta = Ingredient(name="Pasta", category=c.DryGood, est_price=1, quantity=q.Pack)
 
 JalfreziPaste = Ingredient(name="Jalfrezi Paste", category=c.Sauce, est_price=1, quantity=q.Jar)
 MangoChutney = Ingredient(name="Mango Chutney", category=c.Sauce, est_price=1, quantity=q.Jar)
+ThaiCurrySauce = Ingredient(name="Thai Curry Sauce", category=c.Sauce, est_price=1, quantity=q.Jar)
--- Shopping/recipes/ingredients/meats.py
+++ Shopping/recipes/ingredients/meats.py
@@ -1,6 +1,6 @@
-from Shopping.recipes.core import Ingredient, Categories, Quantities
+from Shopping.recipes.core import Categories, Ingredient, Quantities
 
-Bacon = Ingredient(name="Bacon", category=Categories.Meat, est_price=2.50, quantity=Quantities.Pack)
+Bacon = Ingredient(name="Bacon", category=Categories.Meat, est_price=2.50, quantity=12)
 Mince = Ingredient(name="Mince", category=Categories.Meat, est_price=3.0, quantity=Quantities.Pack)
-Chicken = Ingredient(name='Chicken', category=Categories.Meat, est_price=1, quantity=Quantities.Pack)
-Sausage = Ingredient(name='Sausage', category=Categories.Meat, est_price=2, quantity=Quantities.Pack)
+Chicken = Ingredient(name="Chicken", category=Categories.Meat, est_price=1, quantity=6)
+Sausage = Ingredient(name="Sausage", category=Categories.Meat, est_price=2, quantity=8)
